0aee793b35cf0ed070a39e07e47389d63fe423ac
mgnavarrete <mgnavarrete@uc.cl>
2025-01-27 11:37:07 -0300

    header

--- resources/views/layouts/components/landing/header.blade.php
+++ resources/views/layouts/components/landing/header.blade.php
@@ -11,8 +11,8 @@
                         <div class="header-element">
                             <div class="horizontal-logo">
                                 <a href="{{url('index')}}" class="header-logo">
-                                    <img src="{{asset('build/assets/images/brand-logos/toggle-logo.png')}}" alt="logo" class="toggle-logo">
-                                    <img src="{{asset('build/assets/images/brand-logos/toggle-dark.png')}}" alt="logo" class="toggle-dark">
+                                    <img src="{{asset('build/assets/images/brand-logos/toggle-white.png')}}" alt="logo" class="toggle-logo">
+                                    <img src="{{asset('build/assets/images/brand-logos/toggle-white.png')}}" alt="logo" class="toggle-dark">
                                 </a>
                             </div>
                         </div>
--- resources/views/pages/landingIndex.blade.php
+++ resources/views/pages/landingIndex.blade.php
@@ -9,37 +9,10 @@
 
 @section('content')
 
-                    <!-- Start:: Section-1 -->
-                    <div class="landing-banner" id="home">
-                        <section class="section">
-                            <div class="container main-banner-container pb-lg-0">
-                                <div class="row">
-                                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-8">
-                                        <div class="py-lg-5">
-                                            <div class="mb-3">
-                                            
-                                            </div>
-                                            <p class="landing-banner-heading mb-3">Comparte tus imágenes con los novios, familia y amigos.</p>
-                                            <div class="fs-16 mb-5 text-fixed-white op-7">Snap by Minttu.</div>
-
-
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-4">
-                                        <div class="text-end landing-main-image landing-heading-img">
-                                            <img src="{{asset('build/assets/images/takeImg.png')}}" alt="" class="img-fluid">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                    <!-- End:: Section-1 -->
-
                     <!-- Start:: Section-3 -->
                     <section class="section " id="about">
                         <div class="container text-center">
-                            <p class="fs-12 fw-semibold text-success mb-1"><span class="landing-section-heading">Eventos</span></p>
+                            <p class="fs-12 fw-semibold text-success mb-1"><span class="landing-section-heading">Eventos Activos</span></p>
                             <h2 class="fw-semibold mb-2 text-fixed ">Erwin & Fran</h2>
                             <div class="row justify-content-center">
                                 <div class="col-xl-7">
