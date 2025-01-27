fc92535e7be13b0c41a78fb651239dc5d6a13383
mgnavarrete <mgnavarrete@uc.cl>
2025-01-27 12:09:34 -0300

    butons

--- resources/views/pages/eventoShow.blade.php
+++ resources/views/pages/eventoShow.blade.php
@@ -57,11 +57,11 @@
                     </div>
                     <div class="col-xl-12 about-company-stats mb-0">
                         <div class="row justify-content-center">
-                            <div class="col-xxl-5 col-xl-3 col-lg-3 col-md-5 col-sm-8 col-3 ">
+                            <div class="col-xxl-8 col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8 ">
                                 <div class="card custom-card text-default shadow border">
                                     <div class="card-body p-0">
-                                        <div class="row">
-                                            <div class="col-xl-4 col-xxl-4 col-lg-4 col-md-4 col-sm-4 about-company-stats-border">
+                                        <div class="row align-items-center justify-content-center">
+                                            <div class="col-xl-4 col-xxl-4 col-lg-4 col-md-4 col-sm-4 col-4">
                                                 <div class="text-center p-2 w-100 h-100 d-flex align-items-center justify-content-center">
                                                     <div class="d-flex flex-column align-items-center mb-1">
                                                         <a href="" data-bs-toggle="modal" data-bs-target="#agregarImagen">
@@ -71,7 +71,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="col-xl-4 col-xxl-4 col-lg-4 col-md-4 col-sm-4 about-company-stats-border">
+                                            <div class="col-xl-4 col-xxl-4 col-lg-4 col-md-4 col-sm-4 col-4">
                                                 <div class="text-center p-1 w-100 h-100 d-flex align-items-center justify-content-center">
                                                     <div class="d-flex flex-column align-items-center mb-1">
                                                         <a href="">
@@ -81,7 +81,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="col-xl-4 col-xxl-4 col-lg-4 col-md-4 col-sm-4">
+                                            <div class="col-xl-4 col-xxl-4 col-lg-4 col-md-4 col-sm-4 col-4">
                                                 <div class="text-center p-1 w-100 h-100 d-flex align-items-center justify-content-center">
                                                     <div class="d-flex flex-column align-items-center mb-1">
                                                         <a href="" data-bs-toggle="modal" data-bs-target="#agregarComentario">
