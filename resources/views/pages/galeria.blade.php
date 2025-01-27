5925ab149bc656b96ddcde8dbd9c61d0591f71f9
mgnavarrete <mgnavarrete@uc.cl>
2025-01-27 18:54:00 -0300

    drive minttu

--- resources/views/pages/galeria.blade.php
+++ resources/views/pages/galeria.blade.php
@@ -46,8 +46,8 @@
                 $fileId = $imagen->id_google;
                 $directLink = "https://drive.google.com/uc?export=view&id=" . $fileId;
             @endphp
-            <a href="http://mnt-app.ddns.net:5050/proxy?url={{ urlencode($directLink) }}" class="glightbox card" data-gallery="gallery1">
-                <img src="http://mnt-app.ddns.net:5050/proxy?url={{ urlencode($directLink) }}" alt="image" style="width: 100%; height: 100px; object-fit: cover;">
+            <a href="{{$imagen->link}}" class="glightbox card" data-gallery="gallery1">
+                <img src="https://drive.minttu.cl/proxy?url={{ urlencode($directLink) }}" alt="image" style="width: 100%; height: 100px; object-fit: cover;">
             </a>
         </div>
         @endforeach
